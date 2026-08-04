50918060088d34ea920975399c9383436952a296
Faith <1825153451@qq.com>
2026-08-04 16:14:44 +0800

    chore: harden test configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
+  testDir: './tests/e2e',
   use: {
     baseURL: 'http://127.0.0.1:3000',
+    trace: 'retain-on-failure',
   },
   webServer: {
     command: 'pnpm dev',
